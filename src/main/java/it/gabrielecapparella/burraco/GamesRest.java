ee782b0c5a6e6a37a38b44591d684698e8a17c7c
gabrielecapparella <gabriele.capparella@gmail.com>
2020-05-12 15:50:55 +0200

    Updated cards images.

--- src/main/java/it/gabrielecapparella/burraco/GamesRest.java
+++ src/main/java/it/gabrielecapparella/burraco/GamesRest.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-public class GamesRest { // TODO: port to spring
+public class GamesRest {
     private Games gameRepo;
 
     @Autowired
